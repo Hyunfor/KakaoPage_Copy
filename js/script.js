526e39de6b0ad3fcd3add02fa11de0e8e2e76e22
Hyunfor <navel5148@gmail.com>
2022-12-16 16:31:42 +0900

    모바일 화면, 스크롤바 생성 추가

--- js/script.js
+++ js/script.js
@@ -1,16 +1,33 @@
-console.clear();
+function TopBar__init(){
+  // 탑바
+ $('.top-bar .menu-box-1 > ul > li').mouseenter(function(){
+   var $height = $(this).find(' > ul').height();
+   
+   $('.top-bar .menu-box-1 > .bg').addClass('active');
+   $('.top-bar .menu-box-1 > .bg').css('height', $height + 'px');
+ });
+ 
+ $('.top-bar .menu-box-1 > ul > li').mouseleave(function(){
+   var $height = $(this).find(' > ul').height();
+   
+   $('.top-bar .menu-box-1 > .bg').removeClass('active');
+   $('.top-bar .menu-box-1 > .bg').css('height', '0px');
+ });
+ 
+ // 모바일탑바
+ $('.mobile-top-bar .btn-toggle-menu-box-1').click(function(){
 
-function TopBar_init(){
-  $('.top-bar > .con > .menu-box-1 > ul > li').mouseenter(function(){
-    let height = $(this).find(' > ul').height();
-
-    $('.top-bar > .con > .menu-box-1 > .bg').css('height', height);  
-  });
-
-  $('.top-bar > .con > .menu-box-1 > ul > li').mouseleave(function(){
-    $('.top-bar > .con > .menu-box-1 > .bg').css('height', 0);  
-  });
-
+   var $html = $('html'); 
+   var has = $html.hasClass('btn-toggle-menu-box-1-actived');
+   
+   if (has){
+     $html.removeClass('btn-toggle-menu-box-1-actived');
+   }
+   else {
+     $html.addClass('btn-toggle-menu-box-1-actived');
+   }
+   
+ });
 }
 
-TopBar_init();
+TopBar__init();
